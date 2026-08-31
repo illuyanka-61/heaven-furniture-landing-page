5892cad8b2bd3aa6dd90450360c321aaa7af68f7
Sochi <illuyanka61@gmail.com>
2026-08-31 21:09:52 +0600

    Clickable menu button for mobile

---
+++ script.js
@@ -0,0 +1,15 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuToggle.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+
+});
+
+const mobileLinks = document.querySelectorAll(".mobile-menu a");
+
+mobileLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+        mobileMenu.classList.remove("active");
+    });
+});
